42414dee860e148cdec9f666f58fc33872856aea
Naoto Shioya <shioya.naoto17@gmail.com>
2022-01-31 23:27:50 +0900

    add tag logic

--- navigations/TagStackNavigator.tsx
+++ navigations/TagStackNavigator.tsx
@@ -2,11 +2,22 @@ import React, { VFC } from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../types/types';
 import { TagListScreen } from '../screens/TagListScreen';
+import { CreateTagScreen } from '../screens/CreateTagScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const TagStackNavigator: VFC = () => (
   <Stack.Navigator>
-    <Stack.Screen name="TagList" component={TagListScreen} />
+    <Stack.Group>
+      <Stack.Screen name="TagList" component={TagListScreen} />
+    </Stack.Group>
+    <Stack.Group
+      screenOptions={{
+        presentation: 'modal',
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="CreateTag" component={CreateTagScreen} />
+    </Stack.Group>
   </Stack.Navigator>
 );
--- screens/TagListScreen.tsx
+++ screens/TagListScreen.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { VFC } from 'react';
 import tw from 'tailwind-rn';
 import { SafeAreaView, Text, Alert } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
+import { RootStackParamList, Tag } from '../types/types';
 import { auth } from '../firebaseConfig';
 import { selectUser, logout } from '../slices/userSlice';
 import { IconButton } from '../components/IconButton';
+import { Title } from '../components/Title';
 
-export const TagListScreen = () => {
+type Props = {
+  navigation: NativeStackNavigationProp<RootStackParamList, 'TagList'>;
+};
+
+export const TagListScreen: VFC<Props> = ({ navigation }) => {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
 
@@ -21,6 +30,13 @@ export const TagListScreen = () => {
   };
   return (
     <SafeAreaView style={tw('flex-1 mt-5 items-center')}>
+      <Title first="Tag" last="List" />
+      <TouchableOpacity
+        style={tw('mt-2')}
+        onPress={() => navigation.navigate('CreateTag')}
+      >
+        <MaterialCommunityIcons name="tag-plus" size={40} color="#5f9ea0" />
+      </TouchableOpacity>
       <Text>{user.email}</Text>
       <IconButton
         name="logout"
